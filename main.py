5d239115cacdf352bef54051996aec6fcefdae9d
Dewmal Nilanka <dewmalnilanka@gmail.com>
2021-01-02 10:40:15 +0530

    Using render_remplates

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from flask import Flask
+from string import Template
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
+def index():
+    return render_template("index.html", name="AAA")
+
+
+@app.route("/_index_file_from_file_read")
+def using_file_read():
     index_file = open("templates/index.html", "r")
     index_string = index_file.readlines()
-    return f"{''.join(index_string)}"
+    temp_string = Template(''.join(index_string))
+    index_string = temp_string.substitute(name="ABCCC")
+    return f"{index_string}"
 
 
 if __name__ == '__main__':
